dabfaf3c8e10d412ba115d7b11725fe278b8d11a
Franziska Kernbach <fkernbac@1-C-12.42heilbronn.de>
2023-11-01 14:50:11 +0100

    leave queue; frontend IP env; started working on login/API

---
+++ backend/src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserDto } from "./dto";
+
+@Controller('user')
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+}
