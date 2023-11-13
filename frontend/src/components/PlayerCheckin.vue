e0c0d20d55d554f2b8ceafb05fa3083a491cff29
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-11-13 15:18:28 +0100

    Change tournament checkin frontend view 	modified:   frontend/src/components/PlayerCheckin.vue

--- frontend/src/components/PlayerCheckin.vue
+++ frontend/src/components/PlayerCheckin.vue
@@ -1,70 +1,99 @@
 <template>
   <div class="player-checkin">
-    <h5>Players ready: {{ numberOfPlayers }}</h5>
-    <button @click="checkIn" class="press-checkin">{{ btnMsg }}</button>
+    <h5>Players in tournament: {{ 4 - playersInTournament }}</h5>
+    <button @click.prevent="addPlayer" class="add-player">{{ btnMsg }}</button>
   </div>
 </template>
 <script>
   export default {
     data() {
       return {
-        checkMsg: 'Free Place',
-        btnMsg: 'Check-in',
-        checkStatus: false,
-        numberOfPlayers: 0,
-        newPlayername: 'Add Test',
-        newPlayerUniqueId: '100',
+        btnMsg: 'Participate',
+        participateStatus: false,
+        playersInTournament: 0,
       }
+    },
+    async mounted() {
+      try {
+            const response = await fetch(`http://${process.env.VUE_APP_BACKEND_IP}:3000/tournament/all`, {
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            else
+            {
+              const data = await response.json();
+              this.playersInTournament = data["response"];
+            }
+            } catch (error) {
+            console.error('Error getting players in tournament:', error);
+          }
     },
     methods: {
-
-      checkIn() {
-        if (!this.checkStatus) {
-          this.checkStatus = !this.checkStatus;
-          this.numberOfPlayers = this.getNumberofPlayers();
-          this.btnMsg=' Leave ';
-        }
-      },
-
       async addPlayer() {
-      try {
-        const response = await fetch(`http://${process.env.VUE_APP_BACKEND_IP}:3000/tournament/add`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            playerName: this.newPlayerName,
-            playerUniqueId: this.newPlayerUniqueId,
-          }),
-        });
+        /* get the bearer token from local storage and use is as unique player identifier */
+        const storedPlayerToken = localStorage.getItem('userData');
 
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
+        console.log(storedPlayerToken);
+        if (!storedPlayerToken) {
+          console.error('Player token not found in local storage');
+          return;
+        };
+        /* try to add the player to the tournament queue with http post request acting the add method */
+        if (!this.participateStatus) {
+          try {
+            const response = await fetch(`http://${process.env.VUE_APP_BACKEND_IP}:3000/tournament/add`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                playerToken: storedPlayerToken,
+              }),
+            });
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            else
+            {
+              const data = await response.json();
+              this.playersInTournament = data["response"];
+            }
+            this.participateStatus = !this.participateStatus;
+            this.btnMsg = 'Leave Tournament';
+          } catch (error) {
+            console.error('Error adding player:', error);
+          }
         }
-
-        // Refresh the number of players after adding a new player
-        await this.getNumberOfPlayers();
-
-        // Clear the form fields
-        this.newPlayerName = '';
-        this.newPlayerUniqueId = '';
-      } catch (error) {
-        console.error('Error adding player:', error.message);
-      }
-    },
-      async getNumberofPlayers() {
-        try {
-          // console.log(`ENV:${VUE_APP_BACKEND_IP}`);
-          const response = await fetch(`http://${process.env.VUE_APP_BACKEND_IP}:3000/tournament/all`)
-          if (!response.ok)
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          const data = await response.json();
-          this.numberOfPlayers = data.length;
-        } catch (error) {
-          console.error('Error fetching number of players:', error.message);
+        /* try to remove the player from the torunament queue with http delete request acting the remove method */
+        else {
+          try {
+            const response = await fetch(`http://${process.env.VUE_APP_BACKEND_IP}:3000/tournament/${storedPlayerToken}`, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            else {
+              const data = await response.json();
+              this.playersInTournament = data["response"];
+            }
+            // const data = await response.json();
+            // console.log('Player deleted successfully:', data);
+            this.participateStatus = !this.participateStatus;
+            this.btnMsg = 'Participate';
+          } catch (error) {
+            console.error('Error deleting player:', error);
+          }
         }
       },
     },
-  };
+  }
 </script>
