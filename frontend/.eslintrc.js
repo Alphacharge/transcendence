ae2b1eb47e57c442700d810088548d716b170572
fkernbac <fkernbac@Droideka-zBook>
2023-11-13 20:18:21 +0100

    fix for port 8080 error

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -16,5 +16,7 @@ module.exports = {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     'prettier/prettier': 'error',
+	'no-undef': 'off',
+	'no-unreachable': 'off',
   },
 };
