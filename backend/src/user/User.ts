0b488207c2cede6ef998973aa0ac0ca5d2483d81
Alphacharge <rbetz@student.42heilbronn.de>
2023-11-15 11:16:55 +0100

    added userData structure

--- backend/src/user/User.ts
+++ backend/src/user/User.ts
@@ -1,12 +1,14 @@
 import { Socket } from "socket.io";
 import { GameState } from "src/game/GameState";
 import { v4 as uuidv4 } from 'uuid';
+import { Users } from '@prisma/client';
 
 export class User {
   id: number;
   socket: Socket | null;
 
   inGame: boolean;
+  userData: Users;
   gamesPlayed: GameState[]; // REPLACE this with database
 
   constructor() {
