dcbf2f5ed4e47592d188fd2bdd68d51e314c5fcb
Alphacharge <rbetz@student.42heilbronn.de>
2023-11-15 18:07:08 +0100

    added userId to localstorage

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -56,8 +56,11 @@ export default {
 
         const responseData = await response.json();
         if (response.ok) {
-          if (localStorage.getItem("userdata"))
-            localStorage.removeItem("userData");
+          	//maybe not needed anymore
+		  if (localStorage.getItem("access_token"))
+			  localStorage.removeItem("access_token");
+	      if (localStorage.getItem("userId"))
+			  localStorage.removeItem("userId");
           localStorage.setItem("access_token", responseData["access_token"]);
           localStorage.setItem("userId", responseData["userId"]);
           router.push("/");
