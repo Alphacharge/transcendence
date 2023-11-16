5415f241a06b4761bdb0d5ba552a7d3e5c4c72fe
Alphacharge <rbetz@student.42heilbronn.de>
2023-11-16 13:48:55 +0100

    cleaned up user controller and service, removed gamesplayed in user, added userdata object in user

--- backend/src/user/User.ts
+++ backend/src/user/User.ts
@@ -1,17 +1,15 @@
 import { Socket } from "socket.io";
-import { GameState } from "src/game/GameState";
+import { Users } from "@prisma/client";
 import { v4 as uuidv4 } from 'uuid';
 
 export class User {
   id: number;
   socket: Socket | null;
-
   inGame: boolean;
-//   gamesPlayed: GameState[]; // REPLACE this with database
+  userData: Users;
 
   constructor() {
     this.id = uuidv4();
     this.inGame = false;
-    // this.gamesPlayed = [];
   }
 }
