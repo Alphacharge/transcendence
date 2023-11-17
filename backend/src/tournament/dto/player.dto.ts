d50e26ffcc2277d9067cf3cc41267bc982b7a4db
Alphacharge <rbetz@student.42heilbronn.de>
2023-11-17 18:01:13 +0100

    initialized variables, added userData

--- backend/src/tournament/dto/player.dto.ts
+++ backend/src/tournament/dto/player.dto.ts
@@ -1,4 +1,11 @@
+import { Users } from "@prisma/client"
 export class PlayerDto {
 	playerToken: string
 	userId: number
+	userData: Users | undefined = undefined;
+
+	constructor(playerToken: string, userId: number) {
+		this.playerToken = playerToken;
+		this.userId = userId;
+	  }
 }
