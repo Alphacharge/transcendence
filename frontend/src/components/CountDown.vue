3ab73524e30b6fe21e0240085385b50acb7cfded
lsordo | Luca Sordo <lsordo@student.42heilbronn.de>
2023-11-17 16:58:13 +0100

    Add CountDown frontend component 	new file:   frontend/src/components/CountDown.vue

---
+++ frontend/src/components/CountDown.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 v-if="currentCount > 0">{{ currentCount }} ...</h1>
+    <h1 v-else="currentCount === 0">{{ currentCount }} !</h1>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        currentCount: 3,
+        intervalId: null,
+      };
+    },
+    mounted() {
+      this.countDown();
+    },
+    beforeUnmount() {
+      clearInterval(this.intervalId);
+    },
+    methods: {
+      countDown() {
+        this.intervalId = setInterval(
+          ()=>{
+            if (this.currentCount > 0) {
+              this.currentCount--;
+            }
+          }, 1000);}
+      }
+    }
+</script>
