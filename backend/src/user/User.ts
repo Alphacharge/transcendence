783b21a1d7fad79f2f0b774ee4e8b9e3d7e17af3
Alphacharge <rbetz@student.42heilbronn.de>
2023-11-17 19:07:54 +0100

    removed string id, id is in userdata

--- backend/src/user/User.ts
+++ backend/src/user/User.ts
@@ -3,13 +3,13 @@ import { Users } from "@prisma/client";
 import { v4 as uuidv4 } from 'uuid';
 
 export class User {
-  id: string;
   socket: Socket | null;
   inGame: boolean;
-  userData: Users;
+  userData: Users | undefined;
 
   constructor() {
-    this.id = uuidv4();
     this.inGame = false;
+	this.socket = null;
+	this.userData = undefined;
   }
 }
