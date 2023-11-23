31108eaf50cb785d3c7e466ff41138116ea0df1c
Nico marius Kolle <nkolle@3-B-3.42heilbronn.de>
2023-11-23 15:12:45 +0100

    try for responsive Pongfield + elements, needs to be tested in game

--- frontend/src/components/GameArea.vue
+++ frontend/src/components/GameArea.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="container">
     <div class="background" ref="container"></div>
-    <div
-      class="bouncing-ball"
-      :style="{ top: `${bouncingBallY}px`, left: `${bouncingBallX}px` }"
-    ></div>
-    <div class="left-paddle" :style="{ top: `${leftPaddleY}px` }"></div>
-    <div class="right-paddle" :style="{ top: `${rightPaddleY}px` }"></div>
+    <div class="bouncing-ball"></div>
+    <div class="left-paddle"></div>
+    <div class="right-paddle"></div>
   </div>
 </template>
 
@@ -18,16 +15,18 @@ export default {
   props: ["gameId", "playerNumber"],
   data() {
     return {
-      // abll and paddle starting positions
-      bouncingBallX: 400,
-      bouncingBallY: 200,
-      leftPaddleY: 150,
-      rightPaddleY: 150,
+      bouncingBallX: this.containerWidth / 2, // Annahme: Du hast eine Variable für die Breite des Containers (containerWidth)
+      bouncingBallY: this.containerHeight / 2, // Annahme: Du hast eine Variable für die Höhe des Containers (containerHeight)
+      leftPaddleY: this.containerHeight / 2 - 50, // Annahme: Die Paddel sind 100px hoch
+      rightPaddleY: this.containerHeight / 2 - 50, // Annahme: Die Paddel sind 100px hoch
       animationFrameId: null,
       messageInterval: null,
     };
   },
   mounted() {
+
+    this.containerWidth = this.$refs.container.offsetWidth;
+    this.containerHeight = this.$refs.container.offsetHeight;
     // received ball update from server
     socket.on("ballUpdate", (ballCoordinates) => {
       this.bouncingBallX = ballCoordinates.x;
@@ -100,42 +99,50 @@ export default {
 /* Achtung: the field geometry is calculated in the backed, while we set absolute values in the styling, this will now work and needs to be fixed */
 .container {
   position: relative;
-  width: 800px;
-  height: 400px;
+  width: 80vw;  /* 80% der Viewport-Breite */
+  height: 40vh; /* 40% der Viewport-Höhe */
+  /* max-width: 800px;
+  max-height: 400px; */
   overflow: hidden;
 }
+
 
 .background {
   width: 100%;
   height: 100%;
   background-color: rgba(128, 128, 128, 0);
   box-sizing: border-box;
-  border: 10px solid magenta;
+  border: 2px solid magenta;
 }
 
 .bouncing-ball {
-  position: absolute;
+  position: relative;
   width: 10px;
   height: 10px;
   background-color: greenyellow;
   border-radius: 50%;
+  left: 50%;
+  bottom: 50%;
+  transform: translate(50%, 50%);
 }
 
 .left-paddle {
-  position: absolute;
+  position: relative;
   width: 10px;
   height: 100px;
   background-color: blue;
-  left: 40px;
+  left: 5%;
+  bottom: 65%;
   transition: top 0.1s; /* Add a transition for smoother movement */
 }
 
 .right-paddle {
-  position: absolute;
+  position: relative;
   width: 10px;
   height: 100px;
   background-color: rebeccapurple;
-  left: 750px;
+  left: 95%;
+  bottom: 85%;
   transition: top 0.1s; /* Add a transition for smoother movement */
 }
 </style>
