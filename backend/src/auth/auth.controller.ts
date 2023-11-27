877ea4dbc4896c3d0c3e17eb7bde63a1dfb78de4
Luca Sordo <sordo.luca@gmail.com>
2023-11-27 13:06:19 +0100

    Problems with token response  Changes to be committed: 	modified:   auth.controller.ts

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -54,8 +54,38 @@ export class AuthController {
   }
 
   @Get('/42/callback')
-  async handleCallback(@Req() request:Request) {
-    const authorizationCode = request.query.code;
-    console.log(`DEBUG authorizationCode=${authorizationCode}`);
+  async handleCallback(@Req() request: Request) {
+    const authorizationCode = request.query.code as string;
+    /* TODO use environment variables in following instead of actual values */
+    const clientId = 'u-s4t2ud-00df0bcc6de43b6037219a0bdd40cc161fa358149677d5c0036fbe5174a2190b';
+    const clientSecret = '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';
+    const redirectUri = 'https://127.0.0.1:3000/auth/42/callback';
+    const tokenEndpoint = 'https://api.intra.42.fr/oauth/token';
+
+    try {
+      const tokenResponse = await fetch(tokenEndpoint, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          grant_type: authorizationCode,
+          client_id: clientId,
+          client_secret: clientSecret,
+          code: authorizationCode,
+          redirect_uri: redirectUri,
+        }),
+      });
+      if (tokenResponse.ok) {
+        const tokenData = await tokenResponse.json();
+        const accessToken = tokenData.access_token;
+        console.log(`DEBUG accessToken=${accessToken}`)
+      } else {
+        console.error("DEBUG Problems with the tokenresponse");
+      }
+    } catch (error) {
+      // Handle fetch errors
+      console.error("Problems with the tokenresponse");
+    }
   }
 }
