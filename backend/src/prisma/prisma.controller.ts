a050d76f5e081f947a1800a4e59aef59ecba7c13
Alphacharge <rbetz@student.42heilbronn.de>
2023-11-27 13:34:24 +0100

    added friends controller

--- backend/src/prisma/prisma.controller.ts
+++ backend/src/prisma/prisma.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-@Controller('stats')
+@Controller('data')
 export class PrismaController {
   constructor(private readonly prismaService: PrismaService) {}
 
-  @Post('user')
+  @Post('userstats')
   async getHistoryMatches(@Body() body: { userId: number }): Promise<{ userHistory: any[] | null, userProfil: any | null }> {
     const { userId } = body;
     try {
@@ -18,8 +18,20 @@ export class PrismaController {
     }
   }
 
-  @Post('all')
+  @Post('allstats')
   async getUserStatistics(): Promise<any | null> {
     return await this.prismaService.getUserStatistics();
+  }
+
+  @Post('friends')
+  async getFriendsById(@Body() body: { userId: number }): Promise<{ friends: any[] | null }> {
+    const { userId } = body;
+    try {
+      const friends = await this.prismaService.getFriendsById(userId);
+      return { friends };
+    } catch (error) {
+      console.error('Error fetching friends:', error);
+      return { friends: null };
+    }
   }
 }
