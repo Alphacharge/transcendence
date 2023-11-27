0810bf6c6fbe7a0a110f715ae19e4d2954a132f8
Alphacharge <rbetz@student.42heilbronn.de>
2023-11-27 15:17:44 +0100

    added Number casting

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -353,7 +353,7 @@ export class PrismaService extends PrismaClient {
     try {
       const allFriends = await this.friends.findMany({
         where: {
-          user_id: userId,
+          user_id: Number(userId),
         },
         select: {
           friend: {
@@ -377,8 +377,8 @@ export class PrismaService extends PrismaClient {
     try {
       const deletedFriend = await this.friends.deleteMany({
         where: {
-          user_id: userId,
-          friend_id: friendId,
+          user_id: Number(userId),
+          friend_id: Number(friendId),
         },
       });
       return true;
