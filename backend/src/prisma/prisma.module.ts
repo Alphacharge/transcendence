9a2d358623d019cd63fa2850663b930e9c2f5c61
Alphacharge <rbetz@student.42heilbronn.de>
2023-11-28 14:31:51 +0100

    added aut.module

--- backend/src/prisma/prisma.module.ts
+++ backend/src/prisma/prisma.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { PrismaController } from './prisma.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Global()
 @Module({
   controllers: [PrismaController],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, AuthModule],
+  exports: [PrismaService, AuthModule],
 })
 export class PrismaModule {}
