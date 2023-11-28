05d60f6252f2a3d638469b2bd0a20367b40d4926
Nico marius Kolle <nkolle@2-G-8.42heilbronn.de>
2023-11-28 15:51:24 +0100

    updating PongView and Gamearea

--- frontend/src/views/PongView.vue
+++ frontend/src/views/PongView.vue
@@ -1,17 +1,6 @@
 <template>
-  <div class="button-group-container">
-    <PongButtons v-if="pongButtonsVisible" />
-  </div>
   <div>
     <p>I am player number: {{ playerNumber }}</p>
-    <div class="score-board">
-        <div class="left-score">
-          <ScoreBoard :playerScore="player1Score" playerName="Player 1" />
-      </div>
-       <div class="right-score">
-        <ScoreBoard :playerScore="player2Score" playerName="Player 2" />
-        </div>
-      </div>
   </div>
   <CountDown />
   <PongButtons />
@@ -48,39 +37,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.button-group-container {
-  position: absolute;
-  top: 15%; /* Ändere diese Werte entsprechend deiner gewünschten Position */
-  left: 50%;
-  transform: translateX(-50%); /* Zentriert die Button-Gruppe horizontal */
-  width: 40%; /* 100% der Breite des Viewports */
-}
-
-.container {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  /* Füge hier weitere Stile für den Hauptcontainer hinzu, wenn nötig */
-}
-
-.score-board {
-  display:flex;
-  position: absolute;
-  width: calc(100% + 50em);
-  bottom: 12em;
-}
-
-.left-score{
-  flex: 1;
-  position: absolute;
-  left: 8em;
-} 
-.right-score{
-  flex: 1;
-  position: absolute;
-  right: 58em;
-}
-</style>
