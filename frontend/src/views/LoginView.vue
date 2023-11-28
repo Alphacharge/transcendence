25453cf26832238eed59d25ababa90bd13bd64bc
Luca Sordo <sordo.luca@gmail.com>
2023-11-28 12:23:44 +0100

    Anon frontend env variables  Changes to be committed: 	modified:   frontend/src/views/LoginView.vue

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -86,30 +86,23 @@ export default {
       }
     },
     async authorize() {
-      /*TODO: https sollte über .env gehen kriege es aber gerade nicht hin
-      e.g. const authorizationUrl=`${process.env.AUTH_URL}*/
-      // Construct the authorization URL
-      const clientId = 'u-s4t2ud-00df0bcc6de43b6037219a0bdd40cc161fa358149677d5c0036fbe5174a2190b';
-      const redirectUri = 'https://127.0.0.1:3000/auth/42/callback';
-      const scope = 'public profile';
+      const redirectUri = `https://${process.env.VUE_APP_BACKEND_IP}:3000/auth/42/callback`;
+      const scope = `${process.env.VUE_APP_SCOPE}`;
       const authorizationEndpoint = 'https://api.intra.42.fr/oauth/authorize';
-      const state = "dahfg23ut443zu43j5f4vghvtwcvrwhgfasducpds8shdfgdsfkdfskdgfdsfd";
+      const state = `${process.env.VUE_APP_STATE}`;
 
       const queryParams = new URLSearchParams({
-        client_id: clientId,
+        client_id: `${process.env.VUE_APP_FORTYTWO_APP_ID}`,
         redirect_uri: redirectUri,
         scope: scope,
         state: state,
-        response_type: 'code', // requesting an authorization code
-        // Add any additional parameters required by 42
+        response_type: 'code',
       });
       const authorizationUrl = `${authorizationEndpoint}?${queryParams}`;
-      // const authorizationUrl="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-00df0bcc6de43b6037219a0bdd40cc161fa358149677d5c0036fbe5174a2190b&redirect_uri=https%3A%2F%2F127.0.0.1%3A3000%2Fauth%2F42%2Fcallback&response_type=code";
       if (authorizationUrl) {
         window.location.href=authorizationUrl;
-        // console.log(`DEBUG authorizationUrl=${authorizationUrl}`);
       } else {
-        console.error(`DEBUG process.env.AUTH_URL=${authorizationUrl}`);
+        console.error(`LOGIN_VIEW, AUTHORIZE, problems with authorizationUrl: process.env.AUTH_URL=${authorizationUrl}`);
       }
     },
   },
