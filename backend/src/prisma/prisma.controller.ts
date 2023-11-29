67aa1925584b7c5a74830ff734809c0956030ace
Alphacharge <rbetz@student.42heilbronn.de>
2023-11-29 16:42:06 +0100

    added controller to recv the image

--- backend/src/prisma/prisma.controller.ts
+++ backend/src/prisma/prisma.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors, Req, UploadedFile } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { AuthService } from 'src/auth/auth.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { Avatars } from '@prisma/client';
+import { Request } from 'express';
+
+
+export class AvatarCreationFailedException extends HttpException {
+  constructor() {
+    super('Failed to create a new avatar entry', HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
 
 @Controller('data')
 export class PrismaController {
   constructor(
     private readonly prismaService: PrismaService,
     readonly authService: AuthService,
-  ) {}
+    ) {}
 
   @Post('userstats')
   async getHistoryMatches(
@@ -47,5 +60,42 @@ export class PrismaController {
       console.error('Error fetching friends:', error);
       return { friends: null };
     }
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: '../frontend/public/avatars/',
+        filename: async function (req: Request, file, callback) {
+          try {
+            // Get the user ID from the request
+            const userId = req.body.userId;
+  
+            // Create a new avatar entry in the database
+            const avatar: Avatars = await this.prismaService.createNewAvatarById(userId);
+  
+            if (!avatar)
+              throw new AvatarCreationFailedException();
+            // Use the avatar ID as the filename
+            const filename = `${avatar.id}${extname(file.originalname)}`;
+  
+            // Callback with the generated filename
+            callback(null, filename);
+          } catch (error) {
+            // Handle errors
+            callback(error, null);
+          }
+        }.bind(this),
+      }),
+    }),
+  )
+
+  async uploadFile(@Req() req: Request, @UploadedFile() file: Express.Multer.File) {
+    // Access the file information from the request object
+    // Handle the uploaded file here
+    // 'file' contains information about the uploaded file, including the path
+
+    return { filename: file.filename };
   }
 }
