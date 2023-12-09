c3f90bc5bfcec47f28d5a8f64edced4d0d7b2d07
Franziska Kernbach <fkernbac@1-C-12.42heilbronn.de>
2023-12-09 15:14:20 +0100

    fixed local game user2 null errors. websockets are closed with dismount.

--- frontend/src/assets/utils/socket.js
+++ frontend/src/assets/utils/socket.js
@@ -22,6 +22,10 @@ export function connectWebSocket() {
   socket.connect();
 }
 
+export function disconnectWebSocket() {
+  socket.close();
+}
+
 socket.on("connect", () => {
   state.connected = true;
 });
