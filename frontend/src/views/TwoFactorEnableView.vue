6fa999961ae5337cb5594dc09cc448927d3bb2ae
Alphacharge <rbetz@student.42heilbronn.de>
2023-12-12 13:11:45 +0100

    added 2 new views for 2fa

---
+++ frontend/src/views/TwoFactorEnableView.vue
@@ -0,0 +1,13 @@
+<template>
+  <Enable2FA></Enable2FA>
+</template>
+
+<script>
+import Enable2FA from "@/components/Enable2FA.vue";
+
+export default {
+  components: {
+    Enable2FA,
+  },
+};
+</script>
