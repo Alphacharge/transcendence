0747c04632cd44011eeeac97edc7b5e4434d05a9
Alphacharge <rbetz@student.42heilbronn.de>
2023-12-17 14:57:46 +0100

    renamed oathnotpossible in generic error code view

---
+++ frontend/src/views/ErrorView.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="message">
+    <h1>{{ $t("Error") + " " + code }}</h1>
+    <p class="errormessage">{{ errorMessage }}</p>
+    <p>{{ $t("redirect") }}</p>
+    <div v-if="currentCount > 0" class="clock">
+      {{ currentCount }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, onBeforeUnmount } from 'vue';
+import router from "@/router";
+
+export default {
+  props: {
+    code: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    errorMessage() {
+        return this.$t(`error.${this.code}`);
+    },
+  },
+  setup(props) {
+    const currentCount = ref(5);
+
+    const countdownInterval = setInterval(() => {
+      if (currentCount.value > 0) {
+        currentCount.value--;
+      } else {
+        clearInterval(countdownInterval);
+        router.push('/login');
+      }
+    }, 1000);
+
+    onBeforeUnmount(() => {
+      clearInterval(countdownInterval);
+    });
+
+    return {
+      currentCount,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.message {
+  margin-top: 4em;
+  text-align: center;
+  color: rgb(219, 219, 227);
+}
+.errormessage {
+  margin: 2em 0;
+  color: red;
+}
+.clock {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0;
+  text-align: center;
+  font-size: 20em;
+  color: rgb(217, 217, 229);
+  z-index: 2;
+}
+</style>
